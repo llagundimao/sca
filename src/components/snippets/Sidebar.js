2195dd72f6125ab39dd1610351cd1046883cd6e3
Lian Lagundimao <acslian23@gmail.com>
2023-04-18 19:27:05 +0800

    - Added style when item is selected

--- src/components/snippets/Sidebar.js
+++ src/components/snippets/Sidebar.js
@@ -3,7 +3,7 @@ import SCALink from "../../common/data/links.json"
 import { useParams } from "react-router-dom"
 
 const SideBar = () => {
-  const { accountType } = useParams();
+  const { accountType, tab } = useParams();
   const items = accountType === 'seller' ? SCALink.seller : SCALink.buyer || []
 
   return (
@@ -16,16 +16,18 @@ const SideBar = () => {
         <div className="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
           <ul className="space-y-2">
             {items.map((l, i) => {
+              const selectedStyle = l.key === tab ? `bg-blue-400 text-white` : ''
+
               return (
                 <li key={i}>
                   <a
                     href={l.url}
-                    className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                    className={`${selectedStyle} flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-700 group`}
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       className="w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                      fill="currentColor"
+                      fill={`${l.key === tab ? '#000' : 'currentColor'}`}
                       width="14"
                       height="12"
                       viewBox="0 0 20 17"
