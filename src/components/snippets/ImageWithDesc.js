8da0345c4edd27d35d42818037b84b017c50f5ec
llagundimao <acslian23@gmail.com>
2023-04-21 18:38:29 +0800

    - create new landing page

--- src/components/snippets/ImageWithDesc.js
+++ src/components/snippets/ImageWithDesc.js
@@ -3,7 +3,7 @@ import { Image } from '../primitives';
 
 const ImageWithDesc = ({description, image, bgColor}) => {
   return (
-    <div className={`w-1/2 border rounded-lg flex flex-col justify-center p-1 ${bgColor}`} style={{ border: '2px solid #000' }}>
+    <div className={`w-full border text-center rounded-lg flex flex-col justify-center p-1 ${bgColor}`} style={{ border: '3px solid #000' }}>
       <Image src={image} width='w-6'  />
       <p className='text-xs'>{description}</p>
     </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -15,176 +15,155 @@ const Home = () => {
 
   function handleSubmit(event) {
     event.preventDefault()
-    console.log("Username:", username)
-    console.log("Password:", password)
-    navigate('/account-select')
+    setIsLoading(true)
+
     setTimeout(() => {
-      setIsLoading(false)
-      setIsLoading(true)
+      navigate('/account-select')
     }, 2000)
   }
 
   return (
-    <div
-      className="w-full relative flex flex-row justify-center items-center py-12 sm:px-6 lg:px-8 bg-right bg-no-repeat"
-      style={{
-        backgroundImage: `url(${HomeBG})`,
-        backgroundPosition: "right",
-        backgroundColor: "#223347",
-        marginTop: "100px",
-      }}
-    >
+    <>
       <div
-        className="overlay absolute w-full h-full z-10"
-        style={{ backgroundColor: "#223347", opacity: "0.9" }}
-      ></div>
-      <div className="w-3/4 flex z-20">
-        <div className="w-1/2 h-auto flex justify-center align-center items-center">
-          <Image src={SCALogo} alt="ScrapCatApp" height="h-96" />
+        className="w-full relative flex flex-row justify-center items-center py-12 sm:px-6 lg:px-8 bg-right bg-no-repeat"
+        style={{
+          backgroundImage: `url(${HomeBG})`,
+          backgroundPosition: "right",
+          backgroundColor: "#223347",
+          marginTop: "100px",
+        }}
+      >
+        <div
+          className="overlay absolute w-full h-full z-10"
+          style={{ backgroundColor: "#c1d0e2e6" }}
+        ></div>
+        <div className="w-1/2 flex z-20">
+          <div className="w-full h-auto flex justify-center align-center items-center">
+            <Image src={SCALogo} alt="ScrapCatApp" height="h-96" />
+          </div>
         </div>
-        <div className="w-1/2 flex flex-col justify-center items-center">
-          {/* Slogan */}
-          <div className="w-1/2 text-center">
-            <p className="text-2xl text-white">
-              We Do <span className="text-blue-500 italic font-bold">NOT</span>{" "}
-              Buy. We Do{" "}
-              <span className="text-blue-500 italic font-bold">NOT</span> Sell.
-              The{" "}
-              <span className="font-bold italic">
-                Premier Online Auction Marketplace
-              </span>{" "}
-              for{" "}
-              <span className="font-semibold italic">
-                Scrap Catalytic Converter Sales
-              </span>
-            </p>
-          </div>
-          {/* Link Boxes */}
-          <div
-            className="mt-5 text-center flex gap-2 p-2 pl-0 pr-0"
-            style={{ width: "80%" }}
-          >
-            {HomeLinks.home.map((d, i) => (
-              <ImageWithDesc key={i} {...d} />
-            ))}
-          </div>
-          {/* Elements */}
-          <div
-            className="text-center flex p-2 bg-black text-white rounded-lg"
-            style={{ width: "80%" }}
-          >
-            {HomeLinks.element.map((e, i) => (
-              <div key={i} className="flex-col w-3/4">
-                <p className="text-xs font-bold flex justify-center">
-                  <ArrowUp
-                    classes={`w-4 h-4 text-green-500 mr-1 font-extrabold ${
-                      e.status === "down" ? "text-red-500 rotate-180" : ""
-                    }`}
-                  />
-                  <span className="text-yellow-400 font-extrabold">
-                    {e.name}
+        <div className="w-1/2 flex z-20">
+          <div className="w-full flex flex-col">
+            <div className="w-full">
+              {/* Slogan */}
+              <div className="w-full text-center" style={{ margin: '10px auto' }}>
+                <p className="text-2xl">
+                  We Do <span className="text-blue-500 italic font-bold">NOT</span>{" "}
+                  Buy. We Do{" "}
+                  <span className="text-blue-500 italic font-bold">NOT</span> Sell.
+                </p>
+                <p className="text-2xl">
+                  The{" "}
+                  <span className="font-bold italic">
+                    Premier Online Auction Marketplace
+                  </span>{" "}
+                </p>
+                <p className="text-2xl">
+                  for{" "}
+                  <span className="font-semibold italic">
+                    Scrap Catalytic Converter Sales
                   </span>
                 </p>
-                <p className="text-xs font-extrabold">{e.value}</p>
-                <p
-                  className={`text-xs font-extrabold ${
-                    e.status === "up" ? "text-green-500" : "text-red-500"
-                  }`}
-                >
-                  ({e.discrepancy})
+              </div>
+
+              {/* Link Boxes */}
+              <div
+                // className="mt-5 text-center flex gap-2 p-2 pl-0 pr-0"
+                className="mt-2 grid grid-cols-2 gap-2 md:grid-cols-1"
+                style={{ width: "50%", margin: '10px auto' }}
+              >
+                {HomeLinks.home.map((d, i) => (
+                  <ImageWithDesc key={i} {...d} />
+                ))}
+              </div>
+              {/* Elements */}
+              <div
+                className="mt-2 text-center flex justify-right p-2 bg-black text-white rounded-lg"
+                style={{ width: "60%", margin: '10px auto' }}
+              >
+                {HomeLinks.element.map((e, i) => (
+                  <div key={i} className="flex-col w-3/4">
+                    <p className="text-xs font-bold flex justify-center">
+                      <ArrowUp
+                        classes={`w-4 h-4 text-green-500 mr-1 font-extrabold ${e.status === "down" ? "text-red-500 rotate-180" : ""
+                          }`}
+                      />
+                      <span className="text-yellow-400 font-extrabold">
+                        {e.name}
+                      </span>
+                    </p>
+                    <p className="text-xs font-extrabold">{e.value}</p>
+                    <p
+                      className={`text-xs font-extrabold ${e.status === "up" ? "text-green-500" : "text-red-500"
+                        }`}
+                    >
+                      ({e.discrepancy})
+                    </p>
+                  </div>
+                ))}
+              </div>
+              {/* Sales to Date */}
+              <div className="mt-1  text-center" style={{ width: "60%", margin: '10px auto' }}>
+                <p className="mt-2 text-md font-semibold">
+                  ScrapCATapp Marketplace Sales to Date
+                </p>
+                <div className="flex flex-col flex items-center bg-black rounded-lg">
+                  <p className="font-extrabold mt-1 text-xl pb-2 text-green-400">$187,026,662</p>
+                </div>
+                <p className="mt-2 text-md font-bold">
+                  Since 2017
                 </p>
               </div>
-            ))}
-          </div>
-          {/* Sales to Date */}
-          <div className="mt-1  text-center" style={{ width: "80%" }}>
-            <div className="flex flex-col flex bg-black rounded-lg">
-              <p className="mt-2 text-yellow-400 text-sm font-bold">
-                ScrapCATapp Marketplace Sales to Date Since 2017
-              </p>
-              <p className="font-extrabold text-xl pb-2 text-green-400">$187,026,662</p>
             </div>
+          </div>
+          <div className="w-1/4 relative">
+            <Image src='https://www-test.scrapcatapp.com/images/urg%20burst.png' style={{ width: '110%', position: 'absolute', bottom: '0', right: '0' }} />
           </div>
         </div>
       </div>
-      <div className="w-1/4 flex items-center z-20">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <p className="text-2xl font-black text-gray-700 text-center dark:text-white">
-              Signin
-            </p>
-            <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="username"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Username
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="username"
-                    name="username"
-                    type="text"
-                    autoComplete="username"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
+      <div className="w-full text-center flex justify-center items-center">
+        <div className="w-1/2">
+          <p className="text-4xl text-green-600 font-black underline italic">AUCTION <br /> WORKS!</p>
+        </div>
+        <div className="w-1/2 flex flex-row">
+          <div className="w-1/4 flex flex-col justify-center">
+            <div className="text-right text-sm py-2">
+              <a>Forgot Username/Password</a>
+            </div>
+            <div className="text-right text-sm py-3">
+              <a>Not Registered Yet? <span className="underline font-bold">Sign up here!</span></a>
+            </div>
+          </div>
+          <div className="w-3/4 p-2 flex flex-row items-center">
+            <div className="w-3/4">
+              <form id="login-form" onSubmit={handleSubmit}>
+                <div className="flex">
+                  <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+                  </span>
+                  <input value={username} onChange={(event) => setUsername(event.target.value)} required type="text"  className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" />
                 </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <div className="mt-1">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                    required
-                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  />
-                </div>
-              </div>
-              <div>
-                <Button text="Login" isLoading={isLoading} />
-              </div>
-            </form>
-            <div>
-              <p className="mt-2 text-center text-sm text-gray-600">
-                <a
-                  href="#"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot Username / Password
-                </a>
-              </p>
+                <div className="mt-1 flex">
+                  <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
+                    </svg>
 
-              <p className="mt-2 text-center text-sm text-gray-600">
-                Not Registered yet?{" "}
-                <a
-                  href="#"
-                  className="font-medium text-indigo-600 hover:text-indigo-500"
-                >
-                  Signup here!
-                </a>
-              </p>
+                  </span>
+                  <input value={password} onChange={(event) => setPassword(event.target.value)} required type="password" className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" />
+                </div>
+              </form>
+            </div>
+            <div className="w-1/4 flex flex-col justify-center p-5 gap-2">
+              <p className="underline">Contact Us</p>
+              <Button type="submit" form='login-form' text="Login" isLoading={isLoading} />
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="w-1/4 flex items-center z-20">
-        <p className="text-xl text-green-400">AUCTION WORKS!</p>
-      </div> */}
-    </div>
+    </>
   )
 }
 
